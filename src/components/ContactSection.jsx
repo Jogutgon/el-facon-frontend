8e2dfa18fffbb0c5de559fcfe1d29414e90187f0
Josefina Gutierrez Gonzalez <jogugonzalez@gmail.com>
2026-03-21 00:49:03 -0300

    Add: structure, form and tex to the section

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import { Col, Container, Form, Row } from 'react-bootstrap'
+import separator from '../assets/icons/sep.svg'
 
 function ContactSection() {
   return (
-    <div>
-      
-    </div>
+    <section id='contacto'>
+      <img className='sep-icn mt-4 d-block mx-auto' src={separator} alt="separator" />
+
+      <Container className='caja'>
+        <Row>
+          <Col>
+            <h3 className='fira-sans-thin text-center pt-2 pb-3'>Contactos </h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Provident aspernatur quasi accusamus vero nemo nam fugiat.
+              Tempore magni atque sed illo? Perferendis error maiores reiciendis
+              cum commodi tempora laborum facere!</p>
+          </Col>
+          <Col>
+            <Form>
+              <Form.Group className="mb-3" controlId="formGroupName">
+                <Form.Label className='text-white'>Nombre y Apellido</Form.Label>
+                <Form.Control type="text" placeholder="Ingresa tu nombre y apellido" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Label className='text-white'>E-mail</Form.Label>
+                <Form.Control type="email" placeholder="Ingresa tu correo" />
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
