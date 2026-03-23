8d05f6da543eb8490f61d5ed501d021bd39a6c71
Josefina Gutierrez Gonzalez <jogugonzalez@gmail.com>
2026-03-23 17:03:40 -0300

    Add: HomeScreen and RegisterScreen files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import MainSection from './components/MainSection';
-import GalerySection from './components/GalerySection';
-import ContactSection from './components/ContactSection';
-import ReservaSection from './components/ReservaSection';
+
 
 
 
@@ -13,12 +10,9 @@ function App() {
 
   return (
     <>
-      <Header/>
-      <MainSection />
-      <GalerySection/>
-      <ContactSection/>
-      <ReservaSection/>
-      <Footer/>
+      
+
+      
       
       
     </>
---
+++ src/pages/RegisterScreen.jsx
@@ -0,0 +1,13 @@
+
+
+function RegisterScreen() {
+  return (
+    <div className="text-white">
+
+        <h1>Registro</h1>
+      
+    </div>
+  )
+}
+
+export default RegisterScreen
