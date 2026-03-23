df1be3e87562228ac9a974515e8d0a7ef2cd78af
Josefina Gutierrez Gonzalez <jogugonzalez@gmail.com>
2026-03-23 18:02:36 -0300

    Add: structure to the register file, style file

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './pages/HomeScreen';
+import RegisterScreen from './pages/RegisterScreen';
+
+
 
 
 
 
 function App() {
  
-
   return (
     <>
       
+    <Header/>
+    {/* <HomeScreen/> */}
 
+    <RegisterScreen/>
+
+    <Footer/> 
       
-      
-      
+   {/* prox tarea agregar Rutas --> react-router-dom */}
     </>
   )
 }
--- src/pages/RegisterScreen.jsx
+++ src/pages/RegisterScreen.jsx
@@ -1,12 +1,62 @@
+import { Button, Col, Container, Form, Row } from "react-bootstrap"
 
 
 function RegisterScreen() {
   return (
-    <div className="text-white">
 
-        <h1>Registro</h1>
-      
-    </div>
+    <Container className="pt-3 ">
+
+
+      <div className="caja register-page text-white">
+
+        <h2 >Registro</h2>
+        <Row className="d-flex">
+          <Col lg={6} md={6} xs={12}>
+            <Form className="mb-3" >
+              <Form.Group className="mb-3" controlId="formBasicUsername">
+                <Form.Label>Usuario</Form.Label>
+                <Form.Control type="text" placeholder="Agrega un nombre de usuario" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicFirstName">
+                <Form.Label>Nombre</Form.Label>
+                <Form.Control type="text" placeholder=" Ingresa tu nombre" />
+              </Form.Group><Form.Group className="mb-3" controlId="formBasicLastName">
+                <Form.Label>Apellido</Form.Label>
+                <Form.Control type="text" placeholder="Ingresa tu apellido" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Ingresa tu email" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control type="password" placeholder="Ingresa tu contraseña" />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Registrarse
+              </Button>
+
+            </Form>
+
+            <p className="d-inline me-1">¿Ya tenés cuenta?</p>
+            <a href="">Iniciar sesión</a> 
+
+          </Col>
+
+          <Col className="back-image">
+          
+          
+          </Col>
+
+
+        </Row>
+
+      </div>
+    </Container>
+
   )
 }
 
