656892d9e70581b9823110184dd8972c06763f30
Josefina Gutierrez Gonzalez <jogugonzalez@gmail.com>
2026-03-24 15:52:29 -0300

    Add: image and overlay to register

--- src/pages/RegisterScreen.jsx
+++ src/pages/RegisterScreen.jsx
@@ -1,23 +1,23 @@
-import { Button, Col, Container, Form, Row } from "react-bootstrap"
 
+import { Button, Col, Container, Form, Row } from "react-bootstrap"
+import '../styles/RegisterStyles.css'
+import logo from '../assets/icons/logo.svg'
 
 function RegisterScreen() {
   return (
 
-    <Container className="pt-3 ">
-
-
-      <div className="caja register-page text-white">
+    
+    <Container className=" register-page text-white">
 
         <h2 >Registro</h2>
-        <Row className="d-flex">
-          <Col lg={6} md={6} xs={12}>
-            <Form className="mb-3" >
-              <Form.Group className="mb-3" controlId="formBasicUsername">
+        <Row className="">
+          <Col  className=" flex-column">
+            <Form className="mb-2" >
+              <Form.Group className="mb-2" controlId="formBasicUsername">
                 <Form.Label>Usuario</Form.Label>
                 <Form.Control type="text" placeholder="Agrega un nombre de usuario" />
               </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicFirstName">
+              <Form.Group className="mb-2" controlId="formBasicFirstName">
                 <Form.Label>Nombre</Form.Label>
                 <Form.Control type="text" placeholder=" Ingresa tu nombre" />
               </Form.Group><Form.Group className="mb-3" controlId="formBasicLastName">
@@ -25,45 +25,47 @@ function RegisterScreen() {
                 <Form.Control type="text" placeholder="Ingresa tu apellido" />
               </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Group className="mb-2" controlId="formBasicEmail">
                 <Form.Label>Email</Form.Label>
                 <Form.Control type="email" placeholder="Ingresa tu email" />
               </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Group className="mb-2" controlId="formBasicPassword">
                 <Form.Label>Contraseña</Form.Label>
                 <Form.Control type="password" placeholder="Ingresa tu contraseña" />
               </Form.Group>
 
-              <Button variant="primary" type="submit">
+              <Button className="register-button" variant="primary" type="submit">
                 Registrarse
               </Button>
 
             </Form>
-
-            <p className="d-inline me-1">¿Ya tenés cuenta?</p>
-            <a href="">Iniciar sesión</a> 
+            <div className="pb-2">
+              <p className="d-inline me-1 register-text">¿Ya tenés cuenta?</p>
+            <a href="">Iniciar sesión</a>
+            </div>
+             
 
           </Col>
 
-          <Col className="">
+          <Col className='col-img' >
 
-            <div className="register-image">
-              <Container>
-                <h2>Bienvenido</h2>
-              </Container>
+            <div className="register-image ">
+              
+              <div className="register-overlay ">
+                <h1 className="">¡Bienvenido!</h1>
+                <img src={logo} alt="logo" className="img-fluid "/>
+              </div>
+              
             </div>
           
-          
           </Col>
-
-
         </Row>
 
-      </div>
     </Container>
-
   )
 }
 
 export default RegisterScreen
+
+
