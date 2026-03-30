54949092c22d552b173d87f749dffe83810b463c
Josefina Gutierrez Gonzalez <jogugonzalez@gmail.com>
2026-03-30 16:50:39 -0300

    Modify: function UserRouter

--- src/router/UserRouter.jsx
+++ src/router/UserRouter.jsx
@@ -1,23 +1,15 @@
-import React from 'react'
+
 import { Route, Routes } from 'react-router-dom'
 import UserScreen from '../pages/User/UserScreen'
-import PropTypes from 'prop-types'
 
-function UserRouter( { show = false }) {
-  return show ? (
+
+function UserRouter() {
+  return (
     <Routes>
-      <Route path='/' element={<UserScreen/>} />
+      <Route path='/user' element={<UserScreen/>}  />
     </Routes>
-  ) : (
-    <div className='box'>
-
-      <h1 className='text-white'>Usted no tiene permiso de USUARIO</h1>
-    </div>
   )
 }
 
-UserRouter.propTypes= {
-  show: PropTypes.bool.isRequired
-}
 
 export default UserRouter
