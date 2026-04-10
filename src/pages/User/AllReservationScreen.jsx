35c5fe808d630504a0e1241e19f690363d568c4c
Josefina Gutierrez Gonzalez <jogugonzalez@gmail.com>
2026-04-10 16:36:45 -0300

    Add: tab into screen

--- src/pages/User/AllReservationScreen.jsx
+++ src/pages/User/AllReservationScreen.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { Container, Table } from 'react-bootstrap'
 
 function AllReservationScreen() {
   return (
-    <div className='reserv-page'>
+    <Container className='reserv-page text-white'>
       <h1>Mis reservas</h1>
       <p>tablita turururu</p>
-    </div>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Fecha</th>
+            <th>Horario</th>
+            <th>Comensales</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          
+          <tr>
+            <td>1</td>
+            
+          </tr>
+          
+        </tbody>
+      </Table>
+
+
+
+    </Container>
   )
 }
 
