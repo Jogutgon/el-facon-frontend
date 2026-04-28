6b08f9da573be44f13e2776dc9652cd5b98104e6
Josefina Gutierrez Gonzalez <jogugonzalez@gmail.com>
2026-04-28 11:42:28 -0300

    Add: table

--- src/pages/Admin/UserCrudScreen.jsx
+++ src/pages/Admin/UserCrudScreen.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Table } from 'react-bootstrap'
 
 
 function UserCrudScreen() {
+
+
+
+
   return (
     <Container className='my-5 py-5 text-white text-center marco'>
-      <h1 className=''> CRUD DE USUARIOS</h1>
+      <h1 className=''> Usuarios registrados</h1>
+
+      {/* Tabla */}
+
+       <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Usuario</th>
+          <th>Email</th>
+          <th>Estado</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        
+        
+      </tbody>
+    </Table>
+
+
     </Container>
   )
 }
