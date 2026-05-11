f2eb662776b57f6bb6fa5b45dae6d8acfcffe9a0
Josefina Gutierrez Gonzalez <jogugonzalez@gmail.com>
2026-05-10 21:21:49 -0300

    Add: function register Screen

--- src/pages/LoginScreen.jsx
+++ src/pages/LoginScreen.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types'
-import { Button, Col, Container, Form, Row } from 'react-bootstrap'
+import { Button, Col, Container, Form, Row, Toast, ToastContainer } from 'react-bootstrap'
 import '../styles/LoginStyles.css'
 import logo from '../assets/icons/logo.svg'
-import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { jwtDecode } from 'jwt-decode'
 
 
@@ -11,14 +11,16 @@ function LoginScreen({ changeJwt }) {
 
   const navigate = useNavigate();
 
+  const location = useLocation();
+
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [showToast, setShowToast] = useState(false)
 
-  
 
   const handleLogin = (event) => {
     event.preventDefault();
-    
+
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
@@ -36,34 +38,44 @@ function LoginScreen({ changeJwt }) {
 
 
     fetch("http://localhost:7000/auth/login", requestOptions)
-    .then((response) => response.json())
-    .then((result) => {
-      console.log("LOGIN RESULT:", result);
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("LOGIN RESULT:", result);
 
-      if(!result || !result.access_token) {
-        console.error("Token invalido", result);
-        alert("Correo o contraseña incorrectos");
-        return;
-      }
-
-      changeJwt(result.access_token);
-
-      try{
-        const decoded = jwtDecode(result.access_token);
-
-        if (decoded.isAdmin) {
-          navigate('/admin');
-        } else {
-          navigate('/user');
+        if (!result || !result.access_token) {
+          console.error("Token invalido", result);
+          alert("Correo o contraseña incorrectos");
+          return;
         }
-      } catch(error) {
-        console.log("Error decodificando token", error);
-      }
-    })
-    .catch((error) => console.error(error));
-    
-}
- 
+
+        changeJwt(result.access_token);
+
+        try {
+          const decoded = jwtDecode(result.access_token);
+
+          if (decoded.isAdmin) {
+            navigate('/admin');
+          } else {
+            navigate('/user');
+          }
+        } catch (error) {
+          console.log("Error decodificando token", error);
+        }
+      })
+      .catch((error) => console.error(error));
+
+  }
+
+  useEffect(() => {
+    if (location.state?.confirmMessage) {
+      setShowToast(true);
+    }
+  }, [location]);
+
+  const handleToastClose = () => {
+    setShowToast(false);
+  }
+
 
   return (
     <Container className="login-page text-white fira-sans-thin">
@@ -93,14 +105,14 @@ function LoginScreen({ changeJwt }) {
 
             <div className="d-flex justify-content-end">
               <Button className="mt-3" variant="success" type="submit"
-                >Iniciar Sesión</Button>
+              >Iniciar Sesión</Button>
             </div>
           </Form>
 
 
           <div className="d-flex justify-content-center pt-5 login-link">
             <p className="d-inline me-1">¿Aún no tienes cuenta?</p>
-            <Link  to="/register">Registrarse</Link>
+            <Link to="/register">Registrarse</Link>
           </div>
         </Col>
 
@@ -109,9 +121,9 @@ function LoginScreen({ changeJwt }) {
             <div className="login-overlay">
               <h1 className="">¡Bienvenido!</h1>
               <img src={logo} alt="logo" className="img-fluid logo" />
-              
-        
-      
+
+
+
             </div>
           </div>
         </Col>
@@ -119,6 +131,17 @@ function LoginScreen({ changeJwt }) {
       <div className="login-copyright">
         <p>© 2026 Josefina Gutierrez Gonzalez.</p>
       </div>
+
+      <ToastContainer position='bottom-center' className='p-3'>
+        <Toast show={showToast} onClose={handleToastClose} bg='dark'
+          delay={3500} autohide >
+          <Toast.Header className='bg-success d-flex'>
+            <strong className="me-auto text-center">Registro de usuario</strong>
+          </Toast.Header>
+          <Toast.Body> ☑ Registro exitoso. Inicia sesión</Toast.Body>
+        </Toast>
+      </ToastContainer>
+
     </Container>
   )
 }
--- src/pages/RegisterScreen.jsx
+++ src/pages/RegisterScreen.jsx
@@ -2,8 +2,51 @@
 import { Button, Col, Container, Form, Row } from "react-bootstrap"
 import '../styles/RegisterStyles.css'
 import logo from '../assets/icons/logo.svg'
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { API_URL } from "../common/constants"
+import { Link, useNavigate } from "react-router-dom"
+
 
 function RegisterScreen() {
+
+  const [username, setUsername] = useState("")
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const navigate = useNavigate();
+
+  const handleRegister = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(API_URL + '/auth/register',
+      {
+        username,
+        firstName,
+        lastName,
+        email,
+        password
+      });
+
+      console.log(response.data);
+
+      // alert('Registro exitoso');
+
+      navigate('/login', {
+        state: {
+          confirmMessage: 'Registro exitoso. Inicia sesión'
+        }
+      });
+
+    } catch (error) {
+      console.log(error.response.data)
+    };
+  }
+
+
   return (
 
 
@@ -12,25 +55,43 @@ function RegisterScreen() {
       <h3 className="py-1">Registro</h3>
       <Row className="pt-2">
         <Col lg={6} md={12} xs={12} className="container-form mx-auto">
-          <Form className="d-flex flex-column" >
+          <Form
+            className="d-flex flex-column"
+            onSubmit={handleRegister} >
             <Form.Group className="mb-2" controlId="formBasicUsername">
               <Form.Label>Usuario</Form.Label>
-              <Form.Control type="text" placeholder="Agrega un nombre de usuario" />
+              <Form.Control
+                type="text"
+                placeholder="Agrega un nombre de usuario"
+                onChange={(e) => setUsername(e.target.value)}
+                value={username} />
             </Form.Group>
             <Form.Group className="mb-2" controlId="formBasicFirstName">
               <Form.Label>Nombre</Form.Label>
-              <Form.Control type="text" placeholder=" Ingresa tu nombre" />
+              <Form.Control type="text"
+               placeholder=" Ingresa tu nombre"
+               onChange={(e) => setFirstName(e.target.value)}
+               value={firstName} />
             </Form.Group><Form.Group className="mb-2" controlId="formBasicLastName">
               <Form.Label>Apellido</Form.Label>
-              <Form.Control type="text" placeholder="Ingresa tu apellido" />
+              <Form.Control type="text" 
+              placeholder="Ingresa tu apellido"
+              onChange={(e) => setLastName(e.target.value)}
+              value={lastName} />
             </Form.Group>
             <Form.Group className="mb-2" controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Ingresa tu email" />
+              <Form.Control type="email" 
+              placeholder="Ingresa tu email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email} />
             </Form.Group>
             <Form.Group controlId="formBasicPassword">
               <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" placeholder="Ingresa tu contraseña" />
+              <Form.Control type="password" 
+              placeholder="Ingresa tu contraseña"
+              onChange={(e) => setPassword(e.target.value)} 
+              value={password}/>
             </Form.Group>
             <div className="d-flex justify-content-end">
               <Button className="mt-3" variant="success" type="submit">
@@ -40,7 +101,7 @@ function RegisterScreen() {
           </Form>
           <div className="d-flex justify-content-center pt-2 login-link">
             <p className="d-inline me-1 register-text">¿Ya tienes cuenta?</p>
-            <a href="/login">Inicia sesión</a>
+            <Link to="/login">Inicia sesión</Link>
           </div>
         </Col>
 
@@ -55,8 +116,8 @@ function RegisterScreen() {
         </Col>
       </Row>
       <div className="register-copyright">
-              <p>© 2026 Josefina Gutierrez Gonzalez.</p>
-            </div>
+        <p>© 2026 Josefina Gutierrez Gonzalez.</p>
+      </div>
     </Container>
   )
 }
